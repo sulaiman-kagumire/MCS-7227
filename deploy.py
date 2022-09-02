a41d1530ff75ecd896e8b87051cf499e9112933a
Kagumire Sulaiman <56079437+ksulaiman1@users.noreply.github.com>
2022-09-02 19:11:32 +0300

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 # import seaborn as sns
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import StandardScaler, Normalizer
 from sklearn.decomposition import PCA
